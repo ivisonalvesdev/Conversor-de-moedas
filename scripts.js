7179b0c4f01dd8c5e47be05e491cb756a0e7b033
ivisonalvesdev <devivisonalves@gmail.com>
2024-07-31 21:00:44 -0300

    alteração do java script

--- scripts.js
+++ scripts.js
@@ -1,3 +1,39 @@
 const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
-console.log(convertButton)
+function convertValues(){
+
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //valor em real
+    const currencyValueConverted = document.querySelector(".currency-value") // outras moedas (dollar, euro)
+
+    console.log(currencySelect.value)
+
+    const dolarToday = 5.66
+    const euroToday = 6.12
+
+    if(currencySelect.value == "dolar"){
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+
+    }
+
+    if(currencySelect.value == "euro"){
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+        style: "currency",
+        currency: "EUR"
+    }).format(inputCurrencyValue / euroToday)
+
+    }
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+}
+
+
+convertButton.addEventListener("click", convertValues)
